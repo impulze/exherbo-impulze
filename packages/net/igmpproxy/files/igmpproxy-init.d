2790e61d4d8922557d4b88ceca63532cdbc5df8a
Wulf C. Krueger <philantrop@exherbo.org>
2012-05-20 20:40:39 +0200

    igmpproxy: Don't install to/use /var/run.

--- packages/net/igmpproxy/files/igmpproxy-init.d
+++ packages/net/igmpproxy/files/igmpproxy-init.d
@@ -4,22 +4,22 @@
 # $Header: /var/www/viewcvs.gentoo.org/raw_cvs/gentoo-x86/net-misc/igmpproxy/files/igmpproxy-init.d,v 1.1 2009/05/03 20:05:14 idl0r Exp $
 
 depend() {
-	need net
-	use logger
+    need net
+    use logger
 }
 
 start() {
-	ebegin "Starting IGMPproxy"
-	start-stop-daemon --start --background \
-		--make-pidfile --pidfile /var/run/igmpproxy.pid \
-		--exec /usr/sbin/igmpproxy -- \
-		${IGMPPROXY_OPTS} "${IGMPPROXY_CONFIG:-/etc/igmpproxy.conf}"
-	eend $?
+    ebegin "Starting IGMPproxy"
+    start-stop-daemon --start --background \
+        --make-pidfile --pidfile /run/igmpproxy.pid \
+        --exec /usr/sbin/igmpproxy -- \
+        ${IGMPPROXY_OPTS} "${IGMPPROXY_CONFIG:-/etc/igmpproxy.conf}"
+    eend $?
 }
 
 stop() {
-	ebegin "Stopping IGMPproxy"
-	start-stop-daemon --stop --pidfile /var/run/igmpproxy.pid
-	eend $?
+    ebegin "Stopping IGMPproxy"
+    start-stop-daemon --stop --pidfile /run/igmpproxy.pid
+    eend $?
 }
 
